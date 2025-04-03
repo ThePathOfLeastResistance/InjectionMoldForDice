e47e22b2d47059958abebadeb91485d7a1c9b389
James Cao <127159112+ThePathOfLeastResistance@users.noreply.github.com>
2025-04-03 14:20:31 -0400

    added the new changes

--- main.ino
+++ main.ino
@@ -7,31 +7,54 @@ Servo mount2servo;
 Servo baseservo;
 Servo armservo;
 
+int mot = 13;
+
 void setup() {
   moldservo.attach(3);
-  glueservo.attach(5);// attaches the servo on pin 9 to the servo object
-  mount1servo.attach(6);//
-  mount2servo.attach(11);// 
-  baseservo.attach(9);// 
-  armservo.attach(10);// 
-  
+  glueservo.attach(5);
+  mount1servo.attach(6);
+  mount2servo.attach(11);
+  baseservo.attach(9);
+  armservo.attach(10);
+  pinMode(mot, OUTPUT);
 }
 
 void loop() {
+
+
+
+//  Lock the model in,
+  moldservo.write(180);
+  baseservo.write(90);  // TODO: need to figure out the right value    
+  delay(2000);
+
+//  Move down the platform
+  mount1servo.write(0);
+  mount2servo.write(0);
+  delay(3000);        
+
+//Inject the model with Hot glue
+  glueservo.write(180);
+  delay(2000);
+
+//wait 10 seconds for the hot glue to cool
+  delay(12000);
+
+// move the platform up 
   mount1servo.write(180);
   mount2servo.write(180);
-
-  delay(2000);        // waits for the servo to reach the position
-  mount1servo.write(10);
-  mount2servo.write(10);
-  delay(2000);        // waits for the servo to reach the position
-
-  moldservo.write(180);  // sets the servo position according to the scaled value
-  delay(12000);
-  // the move it down 
-  glueservo.write(180);
+  delay(2000);
   
+//Reset the model 
   moldservo.write(0);
-  glueservo.write(0);// sets the servo back to the starting position
-  delay(12000);        // waits for the servo to reach the position
+  glueservo.write(0);
+  delay(2000);  
+
+// Move the model away to the other station
+  armservo.write(90);
+  baseservo.write(90); 
+
+  delay(2000);
+
+  
 }
